8df2e79787f0619dd1c552c3d9e773d11d5e5543
JetAlone <pyrka.marcin@gmail.com>
2017-02-23 22:10:58 +0100

    Init

---
+++ src/Liqster/HomePageBundle/Controller/DefaultController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Liqster\HomePageBundle\Controller;
+
+use Liqster\HomePageBundle\Entity\Foundrise;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Class DefaultController
+ * @package Liqster\HomePageBundle\Controller
+ */
+class DefaultController extends Controller
+{
+    /**
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @throws \LogicException
+     * @Route("/", name="homepage")
+     * @Method({"GET", "POST"})
+     */
+    public function indexAction(Request $request)
+    {
+        return $this->render('LiqsterHomePageBundle:Default:index.html.twig', [
+            'user' => $this->getUser(),
+        ]);
+    }
+}
---
+++ src/Liqster/HomePageBundle/LiqsterHomePageBundle.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Liqster\HomePageBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Class LiqsterHomePageBundle
+ * @package Liqster\HomePageBundle
+ */
+class LiqsterHomePageBundle extends Bundle
+{
+}
