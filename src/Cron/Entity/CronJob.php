261ac3db6b860ded7b503946b668e4b47daedd43
pyrkamarcin <pyrka.marcin@gmail.com>
2017-03-08 11:14:47 +0100

    Update

---
+++ src/Cron/Entity/CronJob.php
@@ -0,0 +1,183 @@
+<?php
+
+namespace Cron\CronBundle\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * CronJob
+ *
+ * @ORM\Table(uniqueConstraints={@ORM\UniqueConstraint(name="un_name", columns={"name"})})
+ * @ORM\Entity(repositoryClass="Cron\CronBundle\Entity\CronJobRepository")
+ */
+class CronJob
+{
+    /**
+     * @ORM\OneToMany(targetEntity="CronReport", mappedBy="job", cascade={"remove"})
+     * @var ArrayCollection
+     */
+    protected $reports;
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="command", type="string", length=255)
+     */
+    private $command;
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="schedule", type="string", length=255)
+     */
+    private $schedule;
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="description", type="string", length=255)
+     */
+    private $description;
+    /**
+     * @var boolean
+     *
+     * @ORM\Column(name="enabled", type="boolean")
+     */
+    private $enabled;
+
+    public function __construct()
+    {
+        $this->reports = new ArrayCollection();
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set name
+     *
+     * @param  string $name
+     * @return CronJob
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCommand()
+    {
+        return $this->command;
+    }
+
+    /**
+     * @param string $command
+     */
+    public function setCommand($command)
+    {
+        $this->command = $command;
+    }
+
+    /**
+     * @return string
+     */
+    public function getSchedule()
+    {
+        return $this->schedule;
+    }
+
+    /**
+     * @param string $schedule
+     */
+    public function setSchedule($schedule)
+    {
+        $this->schedule = $schedule;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set description
+     *
+     * @param  string $description
+     * @return CronJob
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get enabled
+     *
+     * @return boolean
+     */
+    public function getEnabled()
+    {
+        return $this->enabled;
+    }
+
+    /**
+     * Set enabled
+     *
+     * @param  boolean $enabled
+     * @return CronJob
+     */
+    public function setEnabled($enabled)
+    {
+        $this->enabled = $enabled;
+
+        return $this;
+    }
+
+    /**
+     * @return \Doctrine\Common\Collections\ArrayCollection
+     */
+    public function getReports()
+    {
+        return $this->reports;
+    }
+}
---
+++ src/Cron/Entity/CronReport.php
@@ -0,0 +1,136 @@
+<?php
+
+namespace Cron\CronBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * CronReport
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="Cron\CronBundle\Entity\CronReportRepository")
+ */
+class CronReport
+{
+    /**
+     * @ORM\Column(type="datetime")
+     * @var \DateTime $runAt
+     */
+    protected $runAt;
+    /**
+     * @ORM\Column(type="float")
+     * @var float $runTime
+     */
+    protected $runTime;
+    /**
+     * @ORM\Column(type="integer")
+     * @var integer $result
+     */
+    protected $exitCode;
+    /**
+     * @ORM\Column(type="text")
+     * @var string $output
+     */
+    protected $output;
+    /**
+     * @ORM\ManyToOne(targetEntity="CronJob", inversedBy="reports")
+     * @var CronJob
+     */
+    protected $job;
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return CronJob
+     */
+    public function getJob()
+    {
+        return $this->job;
+    }
+
+    /**
+     * @param CronJob $job
+     */
+    public function setJob($job)
+    {
+        $this->job = $job;
+    }
+
+    /**
+     * @return string
+     */
+    public function getOutput()
+    {
+        return $this->output;
+    }
+
+    /**
+     * @param string $output
+     */
+    public function setOutput($output)
+    {
+        $this->output = $output;
+    }
+
+    /**
+     * @return int
+     */
+    public function getExitCode()
+    {
+        return $this->exitCode;
+    }
+
+    /**
+     * @param int $exitCode
+     */
+    public function setExitCode($exitCode)
+    {
+        $this->exitCode = $exitCode;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getRunAt()
+    {
+        return $this->runAt;
+    }
+
+    /**
+     * @param \DateTime $runAt
+     */
+    public function setRunAt($runAt)
+    {
+        $this->runAt = $runAt;
+    }
+
+    /**
+     * @return float
+     */
+    public function getRunTime()
+    {
+        return $this->runTime;
+    }
+
+    /**
+     * @param float $runTime
+     */
+    public function setRunTime($runTime)
+    {
+        $this->runTime = $runTime;
+    }
+}
