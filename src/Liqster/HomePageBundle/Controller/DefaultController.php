bc5ecfbb142a5cd6a642bb7ff20843266de3246d
pyrkamarcin <pyrka.marcin@gmail.com>
2017-03-31 08:08:45 +0200

    Update

--- src/Liqster/HomePageBundle/Controller/DefaultController.php
+++ src/Liqster/HomePageBundle/Controller/DefaultController.php
@@ -14,12 +14,38 @@ class DefaultController extends Controller
 {
     /**
      * @return \Symfony\Component\HttpFoundation\Response
+     * @throws \LogicException
      * @Route("/", name="homepage")
      * @Method({"GET"})
      */
     public function indexAction()
     {
+        if ($this->getUser()) {
+            return $this->redirectToRoute('account_index');
+        }
+
         return $this->render('LiqsterHomePageBundle:Default:index.html.twig', array());
     }
 
+    /**
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @throws \LogicException
+     * @Route("/howtoworks", name="pageHowTo")
+     * @Method({"GET"})
+     */
+    public function pageHowToAction()
+    {
+        return $this->render('LiqsterHomePageBundle:Default:pageHowTo.html.twig', array());
+    }
+
+    /**
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @throws \LogicException
+     * @Route("/contact", name="pageContact")
+     * @Method({"GET"})
+     */
+    public function pageContactAction()
+    {
+        return $this->render('LiqsterHomePageBundle:Default:pageContact.html.twig', array());
+    }
 }
