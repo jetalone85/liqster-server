56775f1c8bc80e3add54e945458373018ebb75de
JetAlone <pyrka.marcin@gmail.com>
2017-04-24 09:05:33 +0200

    Update

---
+++ src/Liqster/HomePageBundle/Controller/ResultController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Liqster\HomePageBundle\Controller;
+
+use Liqster\HomePageBundle\Entity\Account;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ResultController extends Controller
+{
+    /**
+     * Finds and displays a Account entity.
+     *
+     * @Route("/account/{id}/reports", name="result_list")
+     * @Method({"GET"})
+     * @param Account $account
+     * @return Response
+     * @throws \LogicException
+     */
+    public function listAction(Account $account): Response
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $reports = $em->getRepository('CronBundle:CronReport')->findBy(['job' => $account->getCronJob()]);
+
+        $reports = array_slice($reports, -10, 10, true);
+
+        return $this->render('LiqsterHomePageBundle:Result:list.html.twig', array(
+            'reports' => $reports,
+        ));
+    }
+}
