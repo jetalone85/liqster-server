4858cb2e863a3272a4d3b0f8a3c64c8f442fab5b
pyrkamarcin <pyrka.marcin>
2017-06-19 11:34:49 +0200

    Update

--- src/Liqster/PaymentBundle/Controller/DefaultController.php
+++ src/Liqster/PaymentBundle/Controller/DefaultController.php
@@ -5,6 +5,7 @@ namespace Liqster\PaymentBundle\Controller;
 use Psr\Log\LoggerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
 /**
  * Class DefaultController
@@ -17,9 +18,11 @@ class DefaultController extends Controller
     /**
      * @Route("/")
      * @param LoggerInterface $logger
+     * @return Response
      */
-    public function indexAction(LoggerInterface $logger)
+    public function indexAction(LoggerInterface $logger): Response
     {
         $logger->info('I just got the logger');
+        return $this->render('LiqsterPaymentBundle:Default:index.html.twig');
     }
 }
