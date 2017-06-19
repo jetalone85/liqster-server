75a0136995ec60e87fadd84bd03e365ee2a2896f
pyrkamarcin <pyrka.marcin>
2017-06-19 12:40:16 +0200

    Update

---
+++ src/Liqster/PaymentBundle/Entity/Payment.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Liqster\PaymentBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Payment
+ *
+ * @ORM\Table(name="payment")
+ * @ORM\Entity(repositoryClass="Liqster\PaymentBundle\Repository\PaymentRepository")
+ */
+class Payment
+{
+    /**
+     * @var \Ramsey\Uuid\Uuid
+     *
+     * @ORM\Id
+     * @ORM\Column(type="uuid")
+     * @ORM\GeneratedValue(strategy="CUSTOM")
+     * @ORM\CustomIdGenerator(class="Ramsey\Uuid\Doctrine\UuidGenerator")
+     */
+    private $id;
+}
