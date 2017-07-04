b1c6d622c14d482669a4fc92fc65cad08240197e
pyrkamarcin <pyrka.marcin>
2017-07-04 06:59:01 +0200

    Update Views

--- src/Liqster/UserBundle/Controller/RegistrationController.php
+++ src/Liqster/UserBundle/Controller/RegistrationController.php
@@ -6,7 +6,10 @@ use FOS\UserBundle\Controller\RegistrationController as BaseController;
 use FOS\UserBundle\Event\FilterUserResponseEvent;
 use FOS\UserBundle\Event\FormEvent;
 use FOS\UserBundle\Event\GetResponseUserEvent;
+use FOS\UserBundle\Form\Factory\FactoryInterface;
 use FOS\UserBundle\FOSUserEvents;
+use FOS\UserBundle\Model\UserManagerInterface;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 
