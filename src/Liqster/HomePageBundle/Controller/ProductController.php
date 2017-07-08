ed833111f19e1c38ccc9c4edd5396fd9344e43e5
JetAlone <pyrka.marcin@gmail.com>
2017-07-08 23:01:12 +0200

    Update

--- src/Liqster/HomePageBundle/Controller/ProductController.php
+++ src/Liqster/HomePageBundle/Controller/ProductController.php
@@ -6,8 +6,17 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
+/**
+ * Class ProductController
+ * @package Liqster\HomePageBundle\Controller
+ */
 class ProductController extends Controller
 {
+    /**
+     * @param Request $request
+     * @return Response
+     * @throws \LogicException
+     */
     public function fieldAction(Request $request): Response
     {
         $em = $this->getDoctrine();
