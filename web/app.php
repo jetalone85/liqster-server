5dc66b51aed16daab0434d44d751fb2d9aae55d5
pyrkamarcin <pyrka.marcin>
2017-07-11 13:41:57 +0200

    Update

--- web/app.php
+++ web/app.php
@@ -1,5 +1,17 @@
 <?php
 
-//use
+use Symfony\Component\HttpFoundation\Request;
 
-echo 'liqster.pl';
+/** @var \Composer\Autoload\ClassLoader $loader */
+$loader = require __DIR__ . '/../app/autoload.php';
+include_once __DIR__ . '/../var/bootstrap.php.cache';
+
+$kernel = new AppKernel('prod', false);
+$kernel->loadClassCache();
+//$kernel = new AppCache($kernel);
+
+Request::enableHttpMethodParameterOverride();
+$request = Request::createFromGlobals();
+$response = $kernel->handle($request);
+$response->send();
+$kernel->terminate($request, $response);
