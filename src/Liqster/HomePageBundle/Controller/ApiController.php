30d279693801fe74f707872edafbb4f99e976ce9
pyrkamarcin <pyrka.marcin>
2017-07-13 08:52:46 +0200

    Update

--- src/Liqster/HomePageBundle/Controller/ApiController.php
+++ src/Liqster/HomePageBundle/Controller/ApiController.php
@@ -2,7 +2,11 @@
 
 namespace Liqster\HomePageBundle\Controller;
 
+use Liqster\HomePageBundle\Entity\ApiDump;
+use Ramsey\Uuid\Uuid;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -15,7 +19,7 @@ class ApiController extends Controller
 {
     /**
      * @return Response
-     * @Route("/tags.json", name="tags", defaults={"_format": "json"})
+     * @Route("/tags", name="tags", defaults={"_format": "json"})
      * @throws \UnexpectedValueException
      * @throws \LogicException
      */
@@ -24,5 +28,24 @@ class ApiController extends Controller
         $tags = $this->getDoctrine()->getRepository('LiqsterHomePageBundle:Tag')->findBy([], ['name' => 'ASC'], 999);
 
         return $this->render('LiqsterHomePageBundle:Api:tags.json.twig', ['tags' => $tags]);
+    }
+
+    /**
+     * @Route("/tags/input", name="tags_input")
+     * @Method({"GET", "POST"})
+     * @param Request $request
+     * @return Response
+     * @throws \LogicException
+     */
+    public function getTagsAction(Request $request): Response
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $test = new ApiDump();
+        $test->setName(Uuid::uuid1());
+        $em->persist($test);
+        $em->flush();
+
+        return $this->render('LiqsterHomePageBundle:Api:ok.json.twig');
     }
 }
---
+++ src/Liqster/HomePageBundle/Entity/ApiDump.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Liqster\HomePageBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Purchase
+ *
+ * @ORM\Table(name="api_dump")
+ */
+class ApiDump
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column()
+     */
+    protected $name;
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId(int $id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName(string $name)
+    {
+        $this->name = $name;
+    }
+}
