8039af7cc4715101b0b5bdb93d097104a7d08de0
JetAlone <pyrka.marcin@gmail.com>
2017-07-21 18:15:08 +0200

    Revert "Update"
    This reverts commit 98cfb40

--- src/Liqster/Security/Core/User/OAuthUserProvider.php
+++ src/Liqster/Security/Core/User/OAuthUserProvider.php
@@ -4,77 +4,52 @@ namespace Liqster\Security\Core\User;
 
 use HWI\Bundle\OAuthBundle\OAuth\Response\UserResponseInterface;
 use HWI\Bundle\OAuthBundle\Security\Core\User\FOSUBUserProvider as BaseClass;
+use Symfony\Component\Security\Core\User\UserChecker;
 use Symfony\Component\Security\Core\User\UserInterface;
 
+/**
+ * Class OAuthUserProvider
+ * @package AppBundle\Security\Core\User
+ */
 class OAuthUserProvider extends BaseClass
 {
-
-    /**
-     * {@inheritDoc}
-     */
-    public function connect(UserInterface $user, UserResponseInterface $response)
-    {
-        $property = $this->getProperty($response);
-        $username = $response->getUsername();
-
-        //on connect - get the access token and the user ID
-        $service = $response->getResourceOwner()->getName();
-
-        $setter = 'set' . ucfirst($service);
-        $setter_id = $setter . 'Id';
-        $setter_token = $setter . 'AccessToken';
-
-        //we "disconnect" previously connected users
-        if (null !== $previousUser = $this->userManager->findUserBy(array($property => $username))) {
-            $previousUser->$setter_id(null);
-            $previousUser->$setter_token(null);
-            $this->userManager->updateUser($previousUser);
-        }
-
-        //we connect current user
-        $user->$setter_id($username);
-        $user->$setter_token($response->getAccessToken());
-
-        $this->userManager->updateUser($user);
-    }
-
     /**
      * {@inheritdoc}
      */
     public function loadUserByOAuthUserResponse(UserResponseInterface $response)
     {
-        $username = $response->getUsername();
-        $user = $this->userManager->findUserBy(array($this->getProperty($response) => $username));
-        //when the user is registrating
+        $socialID = $response->getUsername();
+        $user = $this->userManager->findUserBy(array($this->getProperty($response) => $socialID));
+        $email = $response->getEmail();
+        //check if the user already has the corresponding social account
         if (null === $user) {
+            //check if the user has a normal account
+            $user = $this->userManager->findUserByEmail($email);
+
+            if (null === $user || !$user instanceof UserInterface) {
+                //if the user does not have a normal account, set it up:
+                $user = $this->userManager->createUser();
+                $user->setEmail($email);
+                $user->setPlainPassword(md5(uniqid()));
+                $user->setEnabled(true);
+            }
+            //then set its corresponding social id
             $service = $response->getResourceOwner()->getName();
-            $setter = 'set' . ucfirst($service);
-            $setter_id = $setter . 'Id';
-            $setter_token = $setter . 'AccessToken';
-            // create new user here
-            $user = $this->userManager->createUser();
-            $user->$setter_id($username);
-            $user->$setter_token($response->getAccessToken());
-            //I have set all requested data with the user's username
-            //modify here with relevant data
-            $user->setUsername($username);
-            $user->setEmail($username);
-            $user->setPassword($username);
-            $user->setEnabled(true);
+            switch ($service) {
+                case 'google':
+                    $user->setGoogleID($socialID);
+                    break;
+                case 'facebook':
+                    $user->setFacebookID($socialID);
+                    break;
+            }
             $this->userManager->updateUser($user);
-            return $user;
+        } else {
+            //and then login the user
+            $checker = new UserChecker();
+            $checker->checkPreAuth($user);
         }
-
-        //if user exists - go with the HWIOAuth way
-        $user = parent::loadUserByOAuthUserResponse($response);
-
-        $serviceName = $response->getResourceOwner()->getName();
-        $setter = 'set' . ucfirst($serviceName) . 'AccessToken';
-
-        //update access token
-        $user->$setter($response->getAccessToken());
 
         return $user;
     }
-
 }
