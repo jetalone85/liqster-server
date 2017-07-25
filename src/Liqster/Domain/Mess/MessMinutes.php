88a21fc14ed935da4cb7389dbac493e7d0d5b93d
pyrkamarcin <pyrka.marcin>
2017-07-25 09:07:32 +0200

    Update

--- src/Liqster/Domain/Mess/MessMinutes.php
+++ src/Liqster/Domain/Mess/MessMinutes.php
@@ -27,7 +27,7 @@ class MessMinutes extends Mess
 
     public static function addMessValue($value)
     {
-        $messValue = random_int(10, 25);
+        $messValue = random_int(2, 5);
         $newValue = (int)$value + $messValue;
 
         if ($newValue > 59) {
