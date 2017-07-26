a5dfa1d95d0e64df4bf05fdfe943a0a66a5b5df7
JetAlone <pyrka.marcin@gmail.com>
2017-07-26 14:31:36 +0200

    Update

--- src/Liqster/HomePageBundle/Controller/NotificationController.php
+++ src/Liqster/HomePageBundle/Controller/NotificationController.php
@@ -8,6 +8,10 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * Class NotificationController
+ * @package Liqster\HomePageBundle\Controller
+ */
 class NotificationController extends Controller
 {
     /**
