f618245ade0f5a3e3efa695947d835cef06d7a10
JetAlone <pyrka.marcin@gmail.com>
2017-08-04 10:33:27 +0200

    Update

---
+++ src/Liqster/Bundle/HomePageBundle/Command/LiqsterPurchaseValidateCommand.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Liqster\Bundle\HomePageBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * Class InstaxerRunCommand
+ *
+ * @package Liqster\Bundle\HomePageBundle\Command
+ */
+class LiqsterPurchaseValidateCommand extends ContainerAwareCommand
+{
+    /**
+     *
+     * @throws \Symfony\Component\Console\Exception\InvalidArgumentException
+     */
+    protected function configure()
+    {
+        $this
+            ->setName('liqster:purchase:validate');
+    }
+
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     * @return int|null|void
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $em = $this->getContainer()->get('doctrine')->getManager();
+        $repository = $em->getRepository('LiqsterHomePageBundle:Account');
+    }
+}
