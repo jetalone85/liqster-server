4d9a7bf45243d99815794a1ff893f05521c8b926
JetAlone <pyrka.marcin@gmail.com>
2017-08-04 10:55:10 +0200

    Update

--- src/Liqster/Bundle/HomePageBundle/Command/LiqsterPurchaseValidateCommand.php
+++ src/Liqster/Bundle/HomePageBundle/Command/LiqsterPurchaseValidateCommand.php
@@ -2,6 +2,7 @@
 
 namespace Liqster\Bundle\HomePageBundle\Command;
 
+use Liqster\Bundle\HomePageBundle\Entity\Purchase;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -27,10 +28,36 @@ class LiqsterPurchaseValidateCommand extends ContainerAwareCommand
      * @param InputInterface $input
      * @param OutputInterface $output
      * @return int|null|void
+     * @throws \Symfony\Component\DependencyInjection\Exception\ServiceNotFoundException
+     * @throws \InvalidArgumentException
+     * @throws \Symfony\Component\DependencyInjection\Exception\ServiceCircularReferenceException
+     * @throws \UnexpectedValueException
+     * @throws \LogicException
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $em = $this->getContainer()->get('doctrine')->getManager();
-        $repository = $em->getRepository('LiqsterHomePageBundle:Account');
+
+        $now = new \DateTime('now');
+
+        $qb = $em->createQueryBuilder();
+        $qb->select('e')
+            ->from(Purchase::class, 'e')
+            ->where('e.paid < :now and e.status = :status')
+            ->setParameter('now', $now)
+            ->setParameter('status', 'verify');
+
+        $purchases = $qb->getQuery()->getResult();
+
+        if ($purchases) {
+            foreach ($purchases as $purchase) {
+
+                $purchase->setStatus('old');
+                $purchase->setModification($now);
+                $em->merge($purchase);
+            }
+        }
+
+        $em->flush();
     }
 }
